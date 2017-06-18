35c910d89c64c12096dc8a048aa5bee8607bab7b
DESKTOP-F9RP5JM\NguyenPham <baonguyenyam@gmail.com>
2017-06-18 16:30:57 +0700

    fixed

--- k-task/task/gulp/local-run.js
+++ k-task/task/gulp/local-run.js
@@ -10,11 +10,31 @@ module.exports = function(gulp, setgulp, plugins, config, target, browserSync) {
 
     // Run task
 
+    gulp.task('local-run-doc', () => {
+
+        return gulp.src([
+                path.join(target, 'docs/**/*.html'),
+                '!' + path.join(target, '*.html')
+            ])
+            .pipe(replace('/css/', '../../css/'))
+            .pipe(replace('/js/', '../../js/'))
+            .pipe(replace('/favicon/', '../../favicon/'))
+            .pipe(replace('/images/', '../../images/'))
+            .pipe(replace('href="//', 'href="@@@'))
+            .pipe(replace('href="/', 'href="../../'))
+            .pipe(replace('href="@@@', 'href="//'))
+            .pipe(replace('../../images/logo.png', '/images/logo.png'))
+            .pipe(replace('../../images/logo.png', '/images/logo.png'))
+            .pipe(replace('//' + config.SEO.cfg_url + '../images/', '//' + config.SEO.cfg_url + '/images/'))
+
+        // .pipe(plugins.changed(dest))
+        .pipe(gulp.dest(dest));
+
+    });
     gulp.task('local-run', () => {
 
         return gulp.src([
-                path.join(target, '**/*.html'),
-                '!' + path.join(target, '*.html')
+                path.join(target, 'docs/*.html')
             ])
             .pipe(replace('/css/', '../css/'))
             .pipe(replace('/js/', '../js/'))
