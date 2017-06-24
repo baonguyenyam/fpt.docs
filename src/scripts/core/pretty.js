bcaeb247e7da1eac1a841e4a296b50baf45c50c6
DESKTOP-F9RP5JM\NguyenPham <baonguyenyam@gmail.com>
2017-06-24 14:46:11 +0700

    up

--- src/scripts/core/pretty.js
+++ src/scripts/core/pretty.js
@@ -2,20 +2,20 @@
 function html(s) {
     return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/#/g, '&#35;').replace(/\\/g, '\\\\').replace(/\n/g, '\<br />').replace(/\[/g, '&lt;').replace(/\]/g, '&gt;').replace(/\*/g, '&ensp;&ensp;');
 }
-
-function htmlLinenum(s) {
-    return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/#/g, '&#35;').replace(/\\/g, '\\\\').replace(/\[/g, '&lt;').replace(/\]/g, '&gt;').replace(/\*/g, '&ensp;&ensp;');
-}
-//]]>
 $('.quine:not(.linenums)').each(function() {
     var newHTML = $(this).html()
     var quineHtml = html(newHTML);
     quineHtml = quineHtml.replace(/&lt;script src[\s\S]*?&gt;&lt;\/script&gt;|&lt;!--\?[\s\S]*?--&gt;|&lt;pre\b[\s\S]*?&lt;\/pre&gt;/g, '<span class="operative">$&</span>');
     $(this).html(quineHtml);
 });
-$('.quine.linenums').each(function() {
-    var newHTML = $(this).html()
-    var quineHtml = htmlLinenum(newHTML);
-    quineHtml = quineHtml.replace(/&lt;script src[\s\S]*?&gt;&lt;\/script&gt;|&lt;!--\?[\s\S]*?--&gt;|&lt;pre\b[\s\S]*?&lt;\/pre&gt;/g, '<span class="operative">$&</span>');
-    $(this).html(quineHtml);
-});
+
+// function htmlLinenum(s) {
+//     return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/#/g, '&#35;').replace(/\\/g, '\\\\').replace(/\[/g, '&lt;').replace(/\]/g, '&gt;').replace(/\*/g, '&ensp;&ensp;');
+// }
+//]]>
+// $('.quine.linenums').each(function() {
+//     var newHTML = $(this).html()
+//     var quineHtml = htmlLinenum(newHTML);
+//     quineHtml = quineHtml.replace(/&lt;script src[\s\S]*?&gt;&lt;\/script&gt;|&lt;!--\?[\s\S]*?--&gt;|&lt;pre\b[\s\S]*?&lt;\/pre&gt;/g, '<span class="operative">$&</span>');
+//     $(this).html(quineHtml);
+// });
