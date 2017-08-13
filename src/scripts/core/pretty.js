1a9b1ada7b0ecdec2673d68c1c863f95bccbc593
DESKTOP-F9RP5JM\NguyenPham <baonguyenyam@gmail.com>
2017-08-13 10:48:19 +0700

    fixed

--- src/scripts/core/pretty.js
+++ src/scripts/core/pretty.js
@@ -3,16 +3,27 @@ function html(s) {
     return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/#/g, '&#35;').replace(/\\/g, '\\\\').replace(/\n/g, '\<br />').replace(/\[/g, '&lt;').replace(/\]/g, '&gt;').replace(/\*/g, '&ensp;&ensp;');
 };
 
+function js(s) {
+    return s.replace(/\n/g, '\<br />').replace(/\^/g, '&ensp;&ensp;');
+};
+
 function css(s) {
     return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/#/g, '&#35;').replace(/\\/g, '\\\\').replace(/\n/g, '\<br />').replace(/\*/g, '&ensp;&ensp;');
 };
-$('.quine:not(.lang-css)').each(function() {
+
+$('.quine.lang-html').each(function() {
     var newHTML = $(this).html()
     var quineHtml = html(newHTML);
     quineHtml = quineHtml.replace(/&lt;script src[\s\S]*?&gt;&lt;\/script&gt;|&lt;!--\?[\s\S]*?--&gt;|&lt;pre\b[\s\S]*?&lt;\/pre&gt;/g, '<span class="operative">$&</span>');
     $(this).html(quineHtml);
 });
-$('.quine:not(.lang-html)').each(function() {
+$('.quine.lang-js').each(function() {
+    var newHTML = $(this).html()
+    var quineHtml = js(newHTML);
+    quineHtml = quineHtml.replace(/&lt;script src[\s\S]*?&gt;&lt;\/script&gt;|&lt;!--\?[\s\S]*?--&gt;|&lt;pre\b[\s\S]*?&lt;\/pre&gt;/g, '<span class="operative">$&</span>');
+    $(this).html(quineHtml);
+});
+$('.quine.lang-css').each(function() {
     var newHTML = $(this).html()
     var quineHtml = css(newHTML);
     quineHtml = quineHtml.replace(/&lt;script src[\s\S]*?&gt;&lt;\/script&gt;|&lt;!--\?[\s\S]*?--&gt;|&lt;pre\b[\s\S]*?&lt;\/pre&gt;/g, '<span class="operative">$&</span>');
